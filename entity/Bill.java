08163c4edf076116d405452a25eb664fe62ca6b9
noone-crlt <ledong04082006@gmail.com>
2025-07-11 22:30:30 +0700

    Update Bill.java

--- entity/Bill.java
+++ entity/Bill.java
@@ -1,19 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package comnieu.entity;
 
-import java.math.BigDecimal;
+import java.util.Date;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ *
+ * @author amyas
+ */
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
 @Builder
-
-public class Receipt {
-    private String receiptId;   // MaPN
-    private String materialId;  // MaNL
-    private Integer quantity;   // SoLuong
-    private BigDecimal price;   // DonGia
+@Data
+public class Bill {
+    private Integer MaHD;
+    private Integer MaNV;
+    @Builder.Default
+    private Date checkin = new Date();
+    private Date checkout;
+    private int status;
+    
+    public static final String DATE_PATTERN = "HH:mm:ss dd-MM-yyyy";
 }
