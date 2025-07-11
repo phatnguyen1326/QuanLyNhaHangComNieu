8848ab3558af5c3f20893af887c6dbb2b02aad41
noone-crlt <ledong04082006@gmail.com>
2025-07-11 22:23:33 +0700

    Update Material.java
    Nguyên Liệu

--- entity/Material.java
+++ entity/Material.java
@@ -1,18 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package comnieu.entity;
 
+import java.util.Date;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ *
+ * @author amyas
+ */
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
 @Builder
-public class Material {
-    private String materialId;
-    private String materialName;
-    private String unit;
-    private String status;       // TrangThai (e.g., "còn", "hết")
-    private String supplierId;
-}
+@Data
+    public class Material {
+        private String MaNL;
+        private String TenNL;
+        private String DonVi;
+        private String TrangThai;
+        private String MaNCC;
+    }
